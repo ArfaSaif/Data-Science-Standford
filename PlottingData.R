1e8a54737d86534641f7a79bbd667ca712ccd157
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-06-28 23:00:00 -0400

    Create PlottingData.R

---
+++ PlottingData.R
@@ -0,0 +1,37 @@
+
+
+# highlight to run
+# using ggplot to visualize data
+
+## import packages
+library(ggplot2)
+
+## use built-in data set for visualization purposes
+## doesn't have missing values
+
+## built in dataset
+mtcars = mtcars
+
+## its a data frame
+
+# select a column
+mtcars$mpg
+
+## order of importance in data science
+## histogram: want to see the distribution of this column: how are these numbers distributed
+## is it a normal distribution:
+
+# plot a histogram to explore the distribution of mpg (first column in dataset)
+
+## empty plot
+## dataset name, name of columns - use plus to add components and attributes to the plot
+## bins is the "histogram bins" along the x axis- divides the ranges of mpg into 10 different ranges
+## counts how many mpgs fall into those ranges
+# find the peak in the plot, ranges that mpg exist
+
+
+ggplot(mtcars,  aes(mpg)) + geom_histogram(bins = 10) 
+
+## add to this
+
+
