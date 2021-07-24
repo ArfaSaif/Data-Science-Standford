aed1af233c7907ff89714c4c7f9984dd1cd24d8a
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-07-24 11:05:09 -0400

    Update lab1.r

--- lab1.r
+++ lab1.r
@@ -26,3 +26,36 @@ ggplot(USArrests, aes(Murder)) + geom_boxplot()
 ## What is the average rate of Murder, Rape in the whole US
 ggplot(USArrests, aes(Murder,Rape)) + geom_point(size=5)
 ## 20
+
+
+##################. NEW DATASET
+library(plotly)
+library(ggplot2)
+LifeCycleSavings = LifeCycleSavings
+
+rownames(LifeCycleSavings)[which.max(LifeCycleSavings$pop15)]
+## Costa Rica
+
+rownames(LifeCycleSavings)[which.max(LifeCycleSavings$pop75)]
+## France
+
+rownames(LifeCycleSavings)[which.max(LifeCycleSavings$dpi)]
+## US
+rownames(LifeCycleSavings)[which.max(LifeCycleSavings$ddpi)]
+## Libya
+
+max(LifeCycleSavings$pop15)
+#47.64
+
+ggplot(LifeCycleSavings, aes(pop75)) + geom_histogram()
+
+ggplot(LifeCycleSavings, aes(pop15, pop75)) + geom_point()
+
+#Exclude the three countries from the data set which have the highest ddpi.
+indexHighDDPI=which.max(LifeCycleSavings$ddpi)
+##~ means to take the complement
+myData <- LifeCycleSavings[-c(indexHighDDPI), ]
+indexHighDDPI=which.max(myData$ddpi)
+myData <- LifeCycleSavings[-c(indexHighDDPI), ]
+indexHighDDPI=which.max(myData$ddpi)
+myData <- LifeCycleSavings[-c(indexHighDDPI), ]
