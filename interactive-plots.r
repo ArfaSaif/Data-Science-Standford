cdd42e4de7272f761805dd74f43f4797b604d407
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-07-24 09:31:49 -0400

    Update interactive-plots.r

--- interactive-plots.r
+++ interactive-plots.r
@@ -1,1 +1,11 @@
 
+
+# interactive plots
+## using a library called plotly
+## install plotly
+library(plotly) ## import library
+
+d <- diamonds[sample(nrow(diamonds), 1000), ]
+plot_ly(d, x = ~carat, y = ~price, color = ~carat, size = ~carat, test = ~paste("Clarity: ", clarity))
+
+
