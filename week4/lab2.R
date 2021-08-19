90170325e46138a1a3403ad3e2e6962345c3d6b8
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-08-19 19:21:23 -0400

    Create lab2.R

---
+++ week4/lab2.R
@@ -0,0 +1,12 @@
+# Download the “Real Estate data Transactions” from Canvas and import it into R. Then, address the following questions:
+ 
+
+# How many unique cities exist in the city column?
+# Is there any correlation between the sq__ft and price? Show the correlation both in number and visually.
+# Use a plot to show the distribution of the prices.
+# Repeat the above task for price and latitude.
+# Is there any correlation between the number of beds and baths?
+# Compare the price ranges for different bed numbers. Which bed number has a higher price in general? Which bed number has a greater fluctuation in price?
+# Repeat the above task for bath number vs. prices.
+# Find the rows which have only one bed.
+# Find the rows which have a sq__ft less than 1000 and at least two beds.
