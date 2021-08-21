18f1c459c48d4efe1ec4c6c58228414093466c4c
Arfa Saif <48233453+ArfaSaif@users.noreply.github.com>
2021-08-21 02:18:31 -0400

    Create lab7.R

---
+++ week7/lab7.R
@@ -0,0 +1,5 @@
+# download zip folder
+# r scripts use to measure all the features you used for trainign the spam model
+# 57 feature columns, there is an R script that runs through the text - plug into the model that we trained to see if its going to classify it as spam or not spam
+
+# if its able to spam or not
